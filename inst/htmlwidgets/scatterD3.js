440c2dd763692f15ab9c0e235d6edd3e26fc126c
Julien Barnier <julien@nozav.org>
2015-12-03 17:26:54 +0100

    Switch to category20() scale if there are more than 10 color levels

--- inst/htmlwidgets/scatterD3.js
+++ inst/htmlwidgets/scatterD3.js
@@ -68,8 +68,9 @@ function scatterD3() {
         x.domain([min_x - gap_x, max_x + gap_x]);
         y.domain([min_y - gap_y, max_y + gap_y]);
         if (settings.colors === null) {
-
-            color_scale = d3.scale.category10();
+            // Number of different levels. See https://github.com/mbostock/d3/issues/472
+            var n = d3.map(data, function(d) { return d.col_var; }).size();
+            color_scale = n <= 10 ? d3.scale.category10() : d3.scale.category20();
         } else if (Array.isArray(settings.colors)) {
             color_scale = d3.scale.ordinal().range(settings.colors);
         } else if (typeof(settings.colors) === "object"){
