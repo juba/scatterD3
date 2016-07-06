ddd725c9caf9f9fe10ff237dc038a1a219150bb4
Julien Barnier <julien@nozav.org>
2016-07-06 17:39:55 +0200

    Add tools menu (reset zoom, SVG export, lasso toggle)

--- inst/htmlwidgets/scatterD3.js
+++ inst/htmlwidgets/scatterD3.js
@@ -148,6 +148,33 @@ function scatterD3() {
 
     }
 
+    // Reset zoom function
+    function reset_zoom() {
+        d3.transition().duration(750).tween("zoom", function() {
+            var ix = d3.interpolate(x.domain(), [min_x - gap_x, max_x + gap_x]),
+                iy = d3.interpolate(y.domain(), [min_y - gap_y, max_y + gap_y]);
+                return function(t) {
+                    zoom.x(x.domain(ix(t))).y(y.domain(iy(t)));
+                    zoomed(reset = true);
+                };
+            });
+    }
+
+    // Export to SVG function
+    function export_svg() {
+        var svg_content = svg
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .attr("version", 1.1)
+        .node().parentNode.innerHTML;
+        svg_content = svg_content.replace(/clip-path="url\(.*?(#.*?)\)"/, 'clip-path="url($1)"');
+        // Dirty dirty dirty...
+        svg_content = svg_content.replace(/<g class="gear-menu".*?<\/g>/, '');
+        var image_data = "data:image/octet-stream;base64,\n" + btoa(svg_content);
+        d3.select(this)
+        .attr("download", "scatterD3.svg")
+        .attr("href", image_data);
+    }
+
     // Create and draw x and y axes
     function add_axes(selection) {
 
@@ -215,7 +242,7 @@ function scatterD3() {
     // Returns dot size from associated data
     function dot_size(data) {
         var size = settings.point_size;
-        if (settings.has_size_var) { size = size_scale(data.size_var) };
+        if (settings.has_size_var) { size = size_scale(data.size_var) }
         return(size)
     }
 
@@ -569,6 +596,19 @@ function scatterD3() {
         pane.style("cursor", "move");
     }
 
+    // Toggle lasso state when element clicked
+    function lasso_toggle() {
+        var el = d3.select(this);
+        if (!el.classed("active") && settings.lasso) {
+            lasso_on(svg);
+        }
+        if (el.classed("active") && settings.lasso) {
+            lasso_off(svg);
+        }
+        var new_state = !el.classed("active")
+        el.classed("active", new_state);
+    }
+
     // Format legend label
     function legend_label_formatting (selection, margin_top) {
         selection
@@ -849,22 +889,53 @@ function scatterD3() {
                 if (settings.has_size_var) add_size_legend(legend);
             }
 
-            // Gear menu
-            gear = svg.append("g")
-            .attr("class", "gear-menu")
-            .attr("width", "25")
-            .attr("height", "25")
-            .style("fill", "#FFFFFF")
-            .attr("transform", "translate(" + (dims.total_width - 25) + "," + 0 + ")");
-            gear.append("rect")
-            .attr("class", "gear-toggle")
-            .attr("width", "25")
-            .attr("height", "25")
-            .style("fill", "#FFFFFF")
-            .append("path")
-            .attr("d", "m 24.28,7.2087374 -1.307796,0 c -0.17052,-0.655338 -0.433486,-1.286349 -0.772208,-1.858846 l 0.927566,-0.929797 c 0.281273,-0.281188 0.281273,-0.738139 0,-1.019312 L 21.600185,1.8728727 C 21.319088,1.591685 20.863146,1.5914219 20.582048,1.8726096 L 19.650069,2.8001358 C 19.077606,2.4614173 18.446602,2.1982296 17.791262,2.0278389 l 0,-1.30783846 c 0,-0.39762 -0.313645,-0.72 -0.711262,-0.72 l -2.16,0 c -0.397618,0 -0.711262,0.32238 -0.711262,0.72 l 0,1.30783846 c -0.65534,0.1703907 -1.286345,0.43344 -1.858849,0.7722138 L 11.420092,1.8724435 c -0.281185,-0.2812846 -0.738131,-0.2812846 -1.019315,0 L 8.8728737,3.3998124 C 8.5916888,3.6809174 8.591427,4.1368574 8.872612,4.4179484 l 0.9275234,0.931984 c -0.3388099,0.572456 -0.6019076,1.203467 -0.7722956,1.858805 l -1.3078398,0 c -0.3976159,0 -0.72,0.313643 -0.72,0.711263 L 7,10.08 c 0,0.397661 0.3223841,0.711263 0.72,0.711263 l 1.3078398,0 c 0.170388,0.655338 0.4334414,1.286349 0.7722084,1.858846 L 8.872349,13.579906 c -0.2811836,0.281105 -0.2811836,0.738139 0,1.019188 l 1.527378,1.527951 c 0.281185,0.28127 0.737041,0.281533 1.018224,3.04e-4 l 0.931981,-0.927484 c 0.572461,0.338718 1.203466,0.601823 1.858806,0.772338 l 0,1.307797 c 0,0.397662 0.313644,0.72 0.711262,0.72 l 2.16,0 c 0.39766,0 0.711262,-0.32238 0.711262,-0.72 l 0,-1.307797 c 0.65534,-0.170515 1.286344,-0.433481 1.858849,-0.772214 l 0.929797,0.927568 c 0.281098,0.281271 0.738131,0.281271 1.019184,0 l 1.527947,-1.527369 c 0.281273,-0.281105 0.281534,-0.737045 3.06e-4,-1.018136 l -0.92748,-0.931984 c 0.338723,-0.572456 0.601819,-1.203467 0.772339,-1.858805 l 1.307796,0 c 0.39766,0 0.72,-0.313643 0.72,-0.711263 l 0,-2.1599996 c 0,-0.39762 -0.322384,-0.711263 -0.72,-0.711263 z M 16,12.6 c -1.988258,0 -3.6,-1.611789 -3.6,-3.5999996 0,-1.988252 1.611742,-3.6 3.6,-3.6 1.988258,0 3.6,1.611748 3.6,3.6 C 19.6,10.988252 17.988258,12.6 16,12.6 Z")
-            .style("fill", "#666666")
-            .style("opacity", "0.1");
+            // Tools menu
+            if(settings.menu) {
+
+              // Gear icon
+              gear = svg.append("g")
+              .attr("class", "gear-menu")
+              .attr("transform", "translate(" + (dims.total_width - 40) + "," + 10 + ")");
+              gear.append("rect")
+              .attr("class", "gear-toggle")
+              .attr("width", "18")
+              .attr("height", "18")
+              .style("fill", "#FFFFFF")
+              gear.append("path")
+              .attr("d", "m 24.28,7.2087374 -1.307796,0 c -0.17052,-0.655338 -0.433486,-1.286349 -0.772208,-1.858846 l 0.927566,-0.929797 c 0.281273,-0.281188 0.281273,-0.738139 0,-1.019312 L 21.600185,1.8728727 C 21.319088,1.591685 20.863146,1.5914219 20.582048,1.8726096 L 19.650069,2.8001358 C 19.077606,2.4614173 18.446602,2.1982296 17.791262,2.0278389 l 0,-1.30783846 c 0,-0.39762 -0.313645,-0.72 -0.711262,-0.72 l -2.16,0 c -0.397618,0 -0.711262,0.32238 -0.711262,0.72 l 0,1.30783846 c -0.65534,0.1703907 -1.286345,0.43344 -1.858849,0.7722138 L 11.420092,1.8724435 c -0.281185,-0.2812846 -0.738131,-0.2812846 -1.019315,0 L 8.8728737,3.3998124 C 8.5916888,3.6809174 8.591427,4.1368574 8.872612,4.4179484 l 0.9275234,0.931984 c -0.3388099,0.572456 -0.6019076,1.203467 -0.7722956,1.858805 l -1.3078398,0 c -0.3976159,0 -0.72,0.313643 -0.72,0.711263 L 7,10.08 c 0,0.397661 0.3223841,0.711263 0.72,0.711263 l 1.3078398,0 c 0.170388,0.655338 0.4334414,1.286349 0.7722084,1.858846 L 8.872349,13.579906 c -0.2811836,0.281105 -0.2811836,0.738139 0,1.019188 l 1.527378,1.527951 c 0.281185,0.28127 0.737041,0.281533 1.018224,3.04e-4 l 0.931981,-0.927484 c 0.572461,0.338718 1.203466,0.601823 1.858806,0.772338 l 0,1.307797 c 0,0.397662 0.313644,0.72 0.711262,0.72 l 2.16,0 c 0.39766,0 0.711262,-0.32238 0.711262,-0.72 l 0,-1.307797 c 0.65534,-0.170515 1.286344,-0.433481 1.858849,-0.772214 l 0.929797,0.927568 c 0.281098,0.281271 0.738131,0.281271 1.019184,0 l 1.527947,-1.527369 c 0.281273,-0.281105 0.281534,-0.737045 3.06e-4,-1.018136 l -0.92748,-0.931984 c 0.338723,-0.572456 0.601819,-1.203467 0.772339,-1.858805 l 1.307796,0 c 0.39766,0 0.72,-0.313643 0.72,-0.711263 l 0,-2.1599996 c 0,-0.39762 -0.322384,-0.711263 -0.72,-0.711263 z M 16,12.6 c -1.988258,0 -3.6,-1.611789 -3.6,-3.5999996 0,-1.988252 1.611742,-3.6 3.6,-3.6 1.988258,0 3.6,1.611748 3.6,3.6 C 19.6,10.988252 17.988258,12.6 16,12.6 Z")
+              .style("fill", "#666666");
+
+              var menu = d3.select(".scatterD3-menu");
+
+              menu.append("li")
+              .append("a")
+              .on("click", reset_zoom)
+              .html("Reset zoom");
+
+              menu.append("li")
+              .append("a")
+              .on("click", export_svg)
+              .html("Export to SVG");
+
+              if (settings.lasso) {
+                menu.append("li")
+                .append("a")
+                .on("click", lasso_toggle)
+                .html("Toggle lasso");
+              }
+
+              gear.on("click", function(d, i){
+                if (menu.style("width") == "0px") {
+                  menu.transition().duration(300)
+                  .style("opacity", "0.95")
+                  .style("width", "150px");
+                } else {
+                  menu.transition().duration(300)
+                  .style("opacity", "0")
+                  .style("width", "0px")
+                }
+              });
+            }
 
         });
     }
@@ -1057,47 +1128,20 @@ function scatterD3() {
 
     };
 
+
     // Add controls handlers for shiny
     chart.add_controls_handlers = function() {
-
         // Zoom reset
         d3.select("#" + settings.dom_id_reset_zoom)
-        .on("click", function() {
-            d3.transition().duration(750).tween("zoom", function() {
-                var ix = d3.interpolate(x.domain(), [min_x - gap_x, max_x + gap_x]),
-                iy = d3.interpolate(y.domain(), [min_y - gap_y, max_y + gap_y]);
-                return function(t) {
-                    zoom.x(x.domain(ix(t))).y(y.domain(iy(t)));
-                    zoomed(reset = true);
-                };
-            })
-        });
+        .on("click", reset_zoom);
 
         // SVG export
         d3.select("#" + settings.dom_id_svg_export)
-        .on("click", function(){
-            var svg_content = svg
-            .attr("xmlns", "http://www.w3.org/2000/svg")
-            .attr("version", 1.1)
-            .node().parentNode.innerHTML;
-            svg_content = svg_content.replace(/clip-path="url\(.*?(#.*?)\)"/,
-                                              'clip-path="url($1)"');
-            var imageUrl = "data:image/octet-stream;base64,\n" + btoa(svg_content);
-            d3.select(this)
-            .attr("download", "scatterD3.svg")
-            .attr("href", imageUrl);
-        });
+        .on("click", export_svg)
 
         // Lasso toggle
         d3.select("#" + settings.dom_id_lasso_toggle)
-        .on("click", function(){
-          if (!d3.select(this).classed("active") && settings.lasso) {
-              lasso_on(svg);
-          }
-          if (d3.select(this).classed("active") && settings.lasso) {
-              lasso_off(svg);
-          }
-        })
+        .on("click", lasso_toggle)
     };
 
     chart.add_global_listeners = function() {
@@ -1206,6 +1250,14 @@ HTMLWidgets.widget({
             .append("div")
             .style("visibility", "hidden")
             .attr("class", "scatterD3-tooltip");
+        }
+
+        // Create tooltip menu div
+        var menu = d3.select(".scatterD3-menu");
+        if (menu.empty()) {
+            menu = d3.select("#htmlwidget_container")
+            .append("ul")
+            .attr("class", "scatterD3-menu");
         }
 
         // Create scatterD3 instance
