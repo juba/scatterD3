115ad736b3b9e09f3f590d04dfd29a369d3607bc
Julien Barnier <julien@nozav.org>
2016-10-24 11:06:23 +0200

    Move labels to external file

---
+++ inst/htmlwidgets/scatterD3-labels.js
@@ -0,0 +1,39 @@
+
+// Initial text label attributes
+function label_init (selection) {
+    selection
+        .attr("text-anchor", "middle");
+}
+
+// Compute default vertical offset for labels
+function default_label_dy(size, y, type_var) {
+    if (y < 0 && type_var !== undefined && type_var == "arrow") {
+        return (Math.sqrt(size) / 2) + settings.labels_size + 2;
+    }
+    else {
+        return (-Math.sqrt(size) / 2) - 6;
+    }
+}
+
+// Apply format to text label
+function label_formatting (selection, settings, scales) {
+    var sel = selection
+        .text(function(d) {return(d.lab);})
+        .style("font-size", settings.labels_size + "px")
+        .attr("class", function(d,i) { return "point-label color color-c" + css_clean(d.col_var) + " symbol symbol-c" + css_clean(d.symbol_var); })
+        .attr("transform", function(d) { return translation(d, scales); })
+        .style("fill", function(d) { return scales.color(d.col_var); })
+        .attr("dx", function(d) {
+	    if (d.lab_dx === undefined) return("0px");
+	    else return(d.lab_dx + "px");
+        })
+        .attr("dy", function(d) {
+	    if (d.lab_dy !== undefined) return(d.lab_dy + "px");
+	    var size = (d.size_var === undefined) ? settings.point_size : scales.size(d.size_var);
+	    return default_label_dy(size, d.y, d.type_var) + "px";
+        });
+    if (settings.opacity_changed || settings.subset_changed || settings.redraw) {
+        sel = sel.style("opacity", 1);
+    }
+    return sel;
+}
--- inst/htmlwidgets/scatterD3.js
+++ inst/htmlwidgets/scatterD3.js
@@ -53,45 +53,6 @@ function scatterD3() {
     }
 
 
-    // Initial text label attributes
-    function label_init (selection) {
-        selection
-            .attr("text-anchor", "middle");
-    }
-
-    // Compute default vertical offset for labels
-    function default_label_dy(size, y, type_var) {
-        if (y < 0 && type_var !== undefined && type_var == "arrow") {
-            return (Math.sqrt(size) / 2) + settings.labels_size + 2;
-        }
-        else {
-            return (-Math.sqrt(size) / 2) - 6;
-        }
-    }
-
-    // Apply format to text label
-    function label_formatting (selection) {
-        var sel = selection
-            .text(function(d) {return(d.lab);})
-            .style("font-size", settings.labels_size + "px")
-            .attr("class", function(d,i) { return "point-label color color-c" + css_clean(d.col_var) + " symbol symbol-c" + css_clean(d.symbol_var); })
-            .attr("transform", function(d) { return translation(d, scales); })
-            .style("fill", function(d) { return scales.color(d.col_var); })
-            .attr("dx", function(d) {
-		if (d.lab_dx === undefined) return("0px");
-		else return(d.lab_dx + "px");
-            })
-            .attr("dy", function(d) {
-		if (d.lab_dy !== undefined) return(d.lab_dy + "px");
-		var size = (d.size_var === undefined) ? settings.point_size : scales.size(d.size_var);
-		return default_label_dy(size, d.y, d.type_var) + "px";
-            });
-        if (settings.opacity_changed || settings.subset_changed || settings.redraw) {
-            sel = sel.style("opacity", 1);
-        }
-        return sel;
-    }
-
     // Text labels dragging function
     var dragging = false;
     drag = d3.drag()
@@ -232,7 +193,7 @@ function scatterD3() {
                 labels.enter()
                     .append("text")
                     .call(label_init)
-                    .call(label_formatting)
+                    .call(function(sel) { label_formatting(sel, settings, scales); })
                     .call(drag);
             }
 
@@ -342,7 +303,7 @@ function scatterD3() {
                 labels.enter()
                     .append("text")
                     .call(label_init)
-                    .call(label_formatting)
+                    .call(function(sel) { label_formatting(sel, settings, scales); })
                     .call(drag);
             }
         }
@@ -444,7 +405,7 @@ function scatterD3() {
             var labels = chart_body.selectAll(".point-label")
 		.data(data, key);
             labels.enter().append("text").call(label_init).call(drag)
-		.merge(labels).transition().duration(1000).call(label_formatting);
+		.merge(labels).transition().duration(1000).call(function(sel) { label_formatting(sel, settings, scales); });
             labels.exit().transition().duration(1000).attr("transform", "translate(0,0)").remove();
 	}
 
