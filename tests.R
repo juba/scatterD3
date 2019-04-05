6f56fd7b3ea407dc1830c494499d9760e83b26a4
Julien <julien@nozav.org>
2019-04-05 10:05:42 +0200

    Add test for many labels

--- tests.R
+++ tests.R
@@ -9,3 +9,8 @@ devtools::install("."); scatterD3(data=mtcars, wt, mpg,
 devtools::install("."); scatterD3(data=mtcars, wt, qsec,
   lab=names, labels_positions="auto")
 
+d <- data.frame(lab = "toto", x = rnorm(100), y = rnorm(100))
+devtools::install("."); scatterD3(data=d, x, y,
+  lab=lab, labels_positions="auto")
+
+
